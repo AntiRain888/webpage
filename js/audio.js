ea965bd5c8e73a44a4e83593982751582c1ff5ec
AntiRain <2987389432@qq.com>
2024-09-19 21:07:50 +0800

    添加了倍速播放的功能 完善了暂停播放键与音量调节的功能

--- js/audio.js
+++ js/audio.js
@@ -2,7 +2,7 @@ const player = document.getElementById('player');
 const playedTime = document.getElementById('playedTime');
 const audioTime = document.getElementById('audioTime');
 // 暂停&播放
-const number=true;
+let isPaused=true;
 const music=document.getElementById("music");
 const pp=document.getElementById("pp");
 
@@ -12,9 +12,11 @@ const skipForward = document.getElementById('skipup');
 const skipBackward = document.getElementById('skipdown');
 
 // 音量调节
-var volume = document.getElementById('volume');
+const volume = document.getElementById('volume');
 // 音量调节滑块
-var volumeTogger = document.getElementById('volumn-togger');
+const volumeTogger = document.getElementById('volumn-togger');
+//倍速
+const speed = document.getElementById('speed');
 
 function formatTime(time) {
   const secondPart = Math.floor(time % 60).toString().padStart(2, '0');
@@ -32,23 +34,23 @@ setInterval(() => {
 
 // 暂停/播放功能
 pp.onclick=function(){
-  if(number===false){
-     number=true;
+  if(isPaused===false){
+     isPaused=true;
     document.getElementById("pp1").style.display="block";
     document.getElementById("pp2").style.display="none";
   
-    music.pause();
+    player.pause();
      
   }else{
     document.getElementById("pp1").style.display="none";
     document.getElementById("pp2").style.display="block";
-    number=false;
-    music.play();
+    isPaused=false;
+    player.play();
   }
 };
 
 // 上一首
-skipup.addEventListener('click', function (event) {
+skipup.addEventListener('click', function () {
   musicId = musicId - 1;
   if (musicId < 0) {
       musicId = 3;
@@ -57,18 +59,13 @@ skipup.addEventListener('click', function (event) {
 });
 
 // 下一首
-skipdown.addEventListener('click', function (event) {
+skipdown.addEventListener('click', function () {
   musicId = musicId + 1;
   if (musicId > 3) {
       musicId = 0;
   }
   initAndPlay();
 });
-
-audio.addEventListener('timeupdate', updateVolumn);
-function updateVolumn() {
-    audio.volume = volumeTogger.value / 70;
-}
 
 
 // 点击音量调节设置静音
@@ -87,3 +84,28 @@ function setNoVolumn() {
         volume.style.backgroundImage = "url('../images/静音.png')";
     }
 }
+
+speed.addEventListener('click', function () {
+  speedText = speed.innerText;
+  if (speedText == "1.0X") {
+      speed.innerText = "1.5X";
+      player.playbackRate = 1.5;
+  }
+  else if (speedText == "1.5X") {
+      speed.innerText = "2.0X";
+      player.playbackRate = 2.0;
+  }
+  else if (speedText == "2.0X") {
+      speed.innerText = "0.5X";
+      player.playbackRate = 0.5;
+  }
+  else if (speedText == "0.5X") {
+      speed.innerText = "1.0X";
+      player.playbackRate = 1.0;
+  }
+});
+
+volumeTogger.addEventListener('change',function(e){
+  console.log(e.target.value/100)
+  player.volume=e.target.value/100
+})
